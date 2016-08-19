e25292c3c3c9045e0d6bed38ae1def1cd15ffa12
Mo Kudeki <mokudekiru@gmail.com>
2016-08-19 15:23:10 -0700

    Change deletion text from 'My Devices' to 'Delete'

--- Code/Controllers/ATLConversationListViewController.m
+++ Code/Controllers/ATLConversationListViewController.m
@@ -49,7 +49,7 @@ static NSString *const ATLGIFMIMETypePlaceholderText = @"Attachment: GIF";
 NSString *const ATLConversationListViewControllerTitle = @"Messages";
 NSString *const ATLConversationTableViewAccessibilityLabel = @"Conversation Table View";
 NSString *const ATLConversationTableViewAccessibilityIdentifier = @"Conversation Table View Identifier";
-NSString *const ATLConversationListViewControllerDeletionModeMyDevices = @"My Devices";
+NSString *const ATLConversationListViewControllerDeletionModeMyDevices = @"Delete";
 NSString *const ATLConversationListViewControllerDeletionModeEveryone = @"Everyone";
 
 + (instancetype)conversationListViewControllerWithLayerClient:(LYRClient *)layerClient
